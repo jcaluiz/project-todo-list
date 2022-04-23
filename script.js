b5d52d262ee773e7b177a4179ec5b43a36b88b2d
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-23 17:13:02 -0300

    requisito 7 concluído

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let button = document.querySelector('#criar-tarefa');
+function caixaDeTextoNaLista() {
+  let button = document.querySelector('#criar-tarefa');
 
-button.addEventListener('click', function() {
-  let createList = document.createElement('li');
-  let ol = document.querySelector('#lista-tarefas');
-  let input = document.querySelector('#texto-tarefa');
-  ol.appendChild(createList);
-  createList.innerText = input.value;
-  input.value = '';
-})
+  button.addEventListener('click', function () {
+    let createList = document.createElement('li');
+    let ol = document.querySelector('#lista-tarefas');
+    let input = document.querySelector('#texto-tarefa');
+    ol.appendChild(createList);
+    createList.innerText = input.value;
+    input.value = '';
+    createList.addEventListener('click', function () {
+      createList.classList.add('back-color')
+    })
+  })
+
+}
+
+caixaDeTextoNaLista();
