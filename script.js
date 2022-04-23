de5469d5d6fd81869c9baaa28cb3861723faaa1a
Luiz Junior <lacjunior129.la@gmail.com>
2022-04-23 14:51:20 -0300

    requisito 5 e 6 concluídos

---
+++ script.js
@@ -0,0 +1,10 @@
+let button = document.querySelector('#criar-tarefa');
+
+button.addEventListener('click', function() {
+  let createList = document.createElement('li');
+  let ol = document.querySelector('#lista-tarefas');
+  let input = document.querySelector('#texto-tarefa');
+  ol.appendChild(createList);
+  createList.innerText = input.value;
+  input.value = '';
+})
